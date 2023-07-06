376f0ad899a6aca1605bc18505af74d9257b73b3
Peggyta <peggyta9096@gmail.com>
2023-07-06 14:00:03 +0330

    set Robota font as global font

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,16 @@
-import '@/styles/globals.css'
+import '@/styles/globals.css';
+import Layout from '@/components/layout/Layout';
+import { Roboto } from '@next/font/google';
+ 
+// If loading a variable font, you don't need to specify the font weight
+const roboto = Roboto({ subsets: ['latin'], weight: ['400', '700'] })
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return( 
+    <main className={roboto.className}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </main>
+  );
 }
