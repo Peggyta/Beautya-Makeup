76bcef3d892eb5cbcfcd40abe6debda3ca09a8d3
Peggyta <peggyta9096@gmail.com>
2023-07-18 12:59:57 +0330

    map on countries/ building branch components

--- components/module/branch/BranchSearchBar.js
+++ components/module/branch/BranchSearchBar.js
@@ -5,22 +5,30 @@ import Near from '@/components/icons/Near';
 
 const BranchSearchBar = () => {
     return (
-        <div className='w-full'>
-            <div className='flex items-center justify-between'>
-                <div className='flex items-center gap-4'>
-                    <input className='w-464 py-2 pl-4 border-b border-cement' placeholder='Address' />
-                    <button className='bg-berry w-44 text-white py-2'>Search</button>
+        <div className='max-w-8xl mx-auto'>
+            <h2 className='text-raven font-bold text-3xl my-3'>Beautya Branches</h2>
+            <div className='w-full flex md:flex-row flex-col items-center justify-between'>
+                <div className='w-full md:w-3/4 flex md:flex-row flex-col items-center lg:gap-4 gap-3'>
+                <div className='text-cement text-sm flex flex-col justify-center lg:w-464 md:w-80 pt-5 w-full '>
+                    <input className='py-3 pl-4 text-lg border-b border-cement' placeholder='Address' />
+                    <p className='mt-1 pl-4'>City, Street Or Zip Code</p>
+                </div>
+                    
+                    <button className='bg-berry hover:bg-neon transition ease-in
+                        lg:w-44 text-white py-2 md:w-36 w-full mt-2 md:mt-0'>Search
+                    </button>
                     <p>Or</p>
-                    <div className='relative'>
-                        <button className='w-44 py-2 text-berry border-2 border-berry'>Around Me</button>
-                        <div className='absolute top-2.5 left-5'><Near /></div>
+                    <div className='relative lg:w-44 md:w-36 w-full '>
+                        <button className='w-full py-2 pl-2 text-berry border-2 w-full border-berry'>Around Me</button>
+                        <div className='absolute top-2.5 lg:left-5 md:left-2 hidden md:block'><Near /></div>
                     </div>
                 </div>
-                <div className='text-raven'>
-                    <Link href='/all-branches'>View All Branches</Link>
+                <div className='text-raven pl-3 w-36 md:my-0 my-8'>
+                    <Link href='/branches/all-branches'>View All Branches</Link>
                 </div>
             </div>
-            <div className='flex justify-between'>
+            
+            <div className='flex justify-between mt-10'>
                 <div>search result</div>
                 <div>branch map</div>
             </div>   
---
+++ components/template/AllBranches.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import data from '@/utils/data';
+import Link from 'next/link';
+
+const AllBranches = () => {
+    return (
+        <div className='max-w-8xl mx-auto px-12 text-raven md:px-10 px-4 bg-dust'>
+            <h2 className='font-bold text-3xl my-3'>All Branches</h2>
+            <p className='md:text-lg text-base font-bold'>Discover Our Branches By Searching For Country Or Region Or Clicking
+            On The Country Lists Below.</p>
+            <div className='w-full flex md:flex-row flex-col items-center justify-between'>
+                <div className='w-full md:w-3/4 flex md:flex-row flex-col items-center lg:gap-4 gap-3'>
+                <div className='text-cement text-sm flex flex-col justify-center lg:w-464 md:w-80 pt-5 w-full '>
+                    <input className='py-3 pl-4 text-lg border-b border-cement' placeholder='Country Or Region' />
+                    <p className='mt-1 pl-4'>City, Street Or Zip Code</p>
+                </div>
+                    <button className='bg-berry hover:bg-neon transition ease-in
+                        lg:w-44 text-white py-2 md:w-36 w-full mt-2 md:mt-0'>Search
+                    </button>
+                </div>
+            </div>
+            <div className='border grid lg:grid-cols-4 md:grid-cols-3  
+            sm:grid-cols-2 grid-cols-1 gap-6 bg-white place-items-center mt-6 py-16'>
+                {data.countries.map((country)=> <div className='flex gap-2 text-raven text-lg'>
+                    <Link href='/'>{country.name}</Link> 
+                    <Link href='/'>({country.amount})</Link>
+                </div>)}
+            </div>
+        </div>
+        
+    );
+};
+
+export default AllBranches;
