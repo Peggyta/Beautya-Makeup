babec6c0889a7b8957c84380efbad649f7537bc3
Peggyta <peggyta9096@gmail.com>
2023-07-31 16:56:03 +0330

    blog json data added

---
+++ utils/blog.js
@@ -0,0 +1,159 @@
+const blog = {
+    blogs: [
+        {
+            title: 'How to get clear skin fast',
+            category: 'Skincare',
+            author: 'Dr. Wade Warren',
+            published: 'Jan 20, 2021',
+            slug: 'mask',
+            description: [
+                'Numerous skin care tips exist for different skin types, and there are also general tips that people can try. Anyone uncertain about their skin care routine should consult a dermatologist for additional advice.',
+                'Many people find it difficult to get clear skin. The methods for getting clear skin will vary, depending on the person`s skin type. In general, people struggling with acne or blemishes have skin that is dry, oily, or a combination of the two.'
+            ],
+            more: [
+                'A pimple indicates trapped oil, sebum, and bacteria. It is also a sign that the body`s natural healing mechanism is working.',
+                'Popping the pimple disrupts this healing process. Additionally, the released liquids expose the surrounding skin to similar bacteria, increasing the risk of additional pimples forming. The exposed pimple may also lead to permanent scarring. Whenever possible, avoid popping pimples. If they drain, keep the area clean.'
+            ],
+            usage: 'The American Academy of Dermatology recommend that people wash twice daily and then again after sweating. Leaving sweat on the skin and allowing it to dry can make acne worse, so it is best to wash the skin as soon as possible after sweating.',
+            image:'../public/images/mask.jpg'
+        },
+        {
+            title: 'How to get clear skin fast',
+            category: 'Skincare',
+            author: 'Dr. Wade Warren',
+            published: 'Jan 20, 2021',
+            slug: 'spray',
+            description: [
+                'Numerous skin care tips exist for different skin types, and there are also general tips that people can try. Anyone uncertain about their skin care routine should consult a dermatologist for additional advice.',
+                'Many people find it difficult to get clear skin. The methods for getting clear skin will vary, depending on the person`s skin type. In general, people struggling with acne or blemishes have skin that is dry, oily, or a combination of the two.'
+            ],
+            more: [
+                'A pimple indicates trapped oil, sebum, and bacteria. It is also a sign that the body`s natural healing mechanism is working.',
+                'Popping the pimple disrupts this healing process. Additionally, the released liquids expose the surrounding skin to similar bacteria, increasing the risk of additional pimples forming. The exposed pimple may also lead to permanent scarring. Whenever possible, avoid popping pimples. If they drain, keep the area clean.'
+            ],
+            usage: 'The American Academy of Dermatology recommend that people wash twice daily and then again after sweating. Leaving sweat on the skin and allowing it to dry can make acne worse, so it is best to wash the skin as soon as possible after sweating.',
+            image: '../public/images/spray.jpg'
+        },
+        {
+            title: 'How to get clear skin fast',
+            category: 'Skincare',
+            author: 'Dr. Wade Warren',
+            published: 'Jan 20, 2021',
+            slug: 'wash',
+            description: [
+                'Numerous skin care tips exist for different skin types, and there are also general tips that people can try. Anyone uncertain about their skin care routine should consult a dermatologist for additional advice.',
+                'Many people find it difficult to get clear skin. The methods for getting clear skin will vary, depending on the person`s skin type. In general, people struggling with acne or blemishes have skin that is dry, oily, or a combination of the two.'
+            ],
+            more: [
+                'A pimple indicates trapped oil, sebum, and bacteria. It is also a sign that the body`s natural healing mechanism is working.',
+                'Popping the pimple disrupts this healing process. Additionally, the released liquids expose the surrounding skin to similar bacteria, increasing the risk of additional pimples forming. The exposed pimple may also lead to permanent scarring. Whenever possible, avoid popping pimples. If they drain, keep the area clean.'
+            ],
+            usage: 'The American Academy of Dermatology recommend that people wash twice daily and then again after sweating. Leaving sweat on the skin and allowing it to dry can make acne worse, so it is best to wash the skin as soon as possible after sweating.',
+            image:'../public/image/wash.jpg'
+        },
+        {
+            title: 'How to get clear skin fast',
+            category: 'Skincare',
+            author: 'Dr. Wade Warren',
+            published: 'Jan 20, 2021',
+            slug: 'brush',
+            description: [
+                'Numerous skin care tips exist for different skin types, and there are also general tips that people can try. Anyone uncertain about their skin care routine should consult a dermatologist for additional advice.',
+                'Many people find it difficult to get clear skin. The methods for getting clear skin will vary, depending on the person`s skin type. In general, people struggling with acne or blemishes have skin that is dry, oily, or a combination of the two.'
+            ],
+            more: [
+                'A pimple indicates trapped oil, sebum, and bacteria. It is also a sign that the body`s natural healing mechanism is working.',
+                'Popping the pimple disrupts this healing process. Additionally, the released liquids expose the surrounding skin to similar bacteria, increasing the risk of additional pimples forming. The exposed pimple may also lead to permanent scarring. Whenever possible, avoid popping pimples. If they drain, keep the area clean.'
+            ],
+            usage: 'The American Academy of Dermatology recommend that people wash twice daily and then again after sweating. Leaving sweat on the skin and allowing it to dry can make acne worse, so it is best to wash the skin as soon as possible after sweating.',
+            image:'../public/image/brush.jpg'
+        },
+        {
+            title: 'How to get clear skin fast',
+            category: 'Skincare',
+            author: 'Dr. Wade Warren',
+            published: 'Jan 20, 2021',
+            slug: 'drink',
+            description: [
+                'Numerous skin care tips exist for different skin types, and there are also general tips that people can try. Anyone uncertain about their skin care routine should consult a dermatologist for additional advice.',
+                'Many people find it difficult to get clear skin. The methods for getting clear skin will vary, depending on the person`s skin type. In general, people struggling with acne or blemishes have skin that is dry, oily, or a combination of the two.'
+            ],
+            more: [
+                'A pimple indicates trapped oil, sebum, and bacteria. It is also a sign that the body`s natural healing mechanism is working.',
+                'Popping the pimple disrupts this healing process. Additionally, the released liquids expose the surrounding skin to similar bacteria, increasing the risk of additional pimples forming. The exposed pimple may also lead to permanent scarring. Whenever possible, avoid popping pimples. If they drain, keep the area clean.'
+            ],
+            usage: 'The American Academy of Dermatology recommend that people wash twice daily and then again after sweating. Leaving sweat on the skin and allowing it to dry can make acne worse, so it is best to wash the skin as soon as possible after sweating.',
+            image:'../public/image/drink.jpg'
+        },
+        {
+            title: 'How to get clear skin fast',
+            category: 'Skincare',
+            author: 'Dr. Wade Warren',
+            published: 'Jan 20, 2021',
+            slug: 'bath',
+            description: [
+                'Numerous skin care tips exist for different skin types, and there are also general tips that people can try. Anyone uncertain about their skin care routine should consult a dermatologist for additional advice.',
+                'Many people find it difficult to get clear skin. The methods for getting clear skin will vary, depending on the person`s skin type. In general, people struggling with acne or blemishes have skin that is dry, oily, or a combination of the two.'
+            ],
+            more: [
+                'A pimple indicates trapped oil, sebum, and bacteria. It is also a sign that the body`s natural healing mechanism is working.',
+                'Popping the pimple disrupts this healing process. Additionally, the released liquids expose the surrounding skin to similar bacteria, increasing the risk of additional pimples forming. The exposed pimple may also lead to permanent scarring. Whenever possible, avoid popping pimples. If they drain, keep the area clean.'
+            ],
+            usage: 'The American Academy of Dermatology recommend that people wash twice daily and then again after sweating. Leaving sweat on the skin and allowing it to dry can make acne worse, so it is best to wash the skin as soon as possible after sweating.',
+            image:'../public/image/bath.jpg'
+        },
+        {
+            title: 'How to get clear skin fast',
+            category: 'Skincare',
+            author: 'Dr. Wade Warren',
+            published: 'Jan 20, 2021',
+            slug: 'flower',
+            description: [
+                'Numerous skin care tips exist for different skin types, and there are also general tips that people can try. Anyone uncertain about their skin care routine should consult a dermatologist for additional advice.',
+                'Many people find it difficult to get clear skin. The methods for getting clear skin will vary, depending on the person`s skin type. In general, people struggling with acne or blemishes have skin that is dry, oily, or a combination of the two.'
+            ],
+            more: [
+                'A pimple indicates trapped oil, sebum, and bacteria. It is also a sign that the body`s natural healing mechanism is working.',
+                'Popping the pimple disrupts this healing process. Additionally, the released liquids expose the surrounding skin to similar bacteria, increasing the risk of additional pimples forming. The exposed pimple may also lead to permanent scarring. Whenever possible, avoid popping pimples. If they drain, keep the area clean.'
+            ],
+            usage: 'The American Academy of Dermatology recommend that people wash twice daily and then again after sweating. Leaving sweat on the skin and allowing it to dry can make acne worse, so it is best to wash the skin as soon as possible after sweating.',
+            image:'../public/image/flower.jpg'
+        },
+        {
+            title: 'How to get clear skin fast',
+            category: 'Skincare',
+            author: 'Dr. Wade Warren',
+            published: 'Jan 20, 2021',
+            slug: 'mirror',
+            description: [
+                'Numerous skin care tips exist for different skin types, and there are also general tips that people can try. Anyone uncertain about their skin care routine should consult a dermatologist for additional advice.',
+                'Many people find it difficult to get clear skin. The methods for getting clear skin will vary, depending on the person`s skin type. In general, people struggling with acne or blemishes have skin that is dry, oily, or a combination of the two.'
+            ],
+            more: [
+                'A pimple indicates trapped oil, sebum, and bacteria. It is also a sign that the body`s natural healing mechanism is working.',
+                'Popping the pimple disrupts this healing process. Additionally, the released liquids expose the surrounding skin to similar bacteria, increasing the risk of additional pimples forming. The exposed pimple may also lead to permanent scarring. Whenever possible, avoid popping pimples. If they drain, keep the area clean.'
+            ],
+            usage: 'The American Academy of Dermatology recommend that people wash twice daily and then again after sweating. Leaving sweat on the skin and allowing it to dry can make acne worse, so it is best to wash the skin as soon as possible after sweating.',
+            image:'../public/image/mirror.jpg'
+        },
+        {
+            title: 'How to get clear skin fast',
+            category: 'Skincare',
+            author: 'Dr. Wade Warren',
+            published: 'Jan 20, 2021',
+            slug: 'filler',
+            description: [
+                'Numerous skin care tips exist for different skin types, and there are also general tips that people can try. Anyone uncertain about their skin care routine should consult a dermatologist for additional advice.',
+                'Many people find it difficult to get clear skin. The methods for getting clear skin will vary, depending on the person`s skin type. In general, people struggling with acne or blemishes have skin that is dry, oily, or a combination of the two.'
+            ],
+            more: [
+                'A pimple indicates trapped oil, sebum, and bacteria. It is also a sign that the body`s natural healing mechanism is working.',
+                'Popping the pimple disrupts this healing process. Additionally, the released liquids expose the surrounding skin to similar bacteria, increasing the risk of additional pimples forming. The exposed pimple may also lead to permanent scarring. Whenever possible, avoid popping pimples. If they drain, keep the area clean.'
+            ],
+            usage: 'The American Academy of Dermatology recommend that people wash twice daily and then again after sweating. Leaving sweat on the skin and allowing it to dry can make acne worse, so it is best to wash the skin as soon as possible after sweating.',
+            image:'../public/image/filler.jpg'
+        }
+    ]
+}
+
+export default blog;
