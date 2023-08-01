d1dec8a16d6da878d01932129007645adcede98e
Peggyta <peggyta9096@gmail.com>
2023-08-01 16:09:27 +0330

    get data from database and pass it as a props to Blog component

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,11 +1,29 @@
 import React from 'react';
+import Blog from '@/components/template/Blog';
+import BlogModel from '@/models/BlogModel';
+import connectDB from '@/utils/connectDB';
 
-const AllBlogs = () => {
+const AllBlogs = ({blogs}) => {
+    console.log(blogs);
     return (
         <div>
-            
+            <Blog data={blogs} />
         </div>
     );
 };
 
 export default AllBlogs;
+
+export async function getServerSideProps() {
+    try {
+        await connectDB();
+        const blogs = await BlogModel.find();
+        return {
+            props: {blogs: JSON.parse(JSON.stringify(blogs))}
+        }
+    } catch(err) {
+        return {
+            notFound: true,
+        }
+    }
+}
