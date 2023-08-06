0f4fef2683877b8a1c2de0bd63aadeb84b5f93d3
Peggyta <peggyta9096@gmail.com>
2023-08-06 13:53:13 +0330

    add styles/ create filter component for products

--- components/module/makeup/ProductCard.js
+++ components/module/makeup/ProductCard.js
@@ -1,14 +1,17 @@
-import React, {useState} from 'react';
+import React from 'react';
 
 const ProductCard = (props) => {
-    const [showPrice, setShowPrice] = useState(false);
     const{title, price, description, slug} = props;
     return (
-        <div className='w-72 border mx-auto border-ash text-raven'>
-                <img  className='w-full' src={`/images/${slug}.jpg`} alt='product' />
-                <h3 className='text-lily font-bold'>{title}</h3>
-                <p className='text-sm'>{description}</p>
-                <p className= {price>0 ? 'text-lg': 'hidden'}>{price} $</p>
+        <div className='sm:w-72 w-56 flex flex-col justify-between border mx-auto border-ash text-raven bg-white'>
+            <div>
+                <img className='w-full h-80 object-cover' src={`/images/${slug}.jpg`} alt='product' />
+                <h3 className='text-lily font-bold pt-3 pb-2 px-2'>{title}</h3>
+                <p className='text-sm px-2'>{description}</p>
+            </div>
+            <div className='pl-2 pb-2'>
+                <p className= {price>0 ? 'text-lg': 'hidden'}>$ {price}</p>
+            </div>       
         </div>
     );
 };
---
+++ components/module/makeup/SortProducts.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const SortProducts = () => {
+    return (
+        <div className='max-w-8xl mx-auto'>
+        <div className='flex justify-between flex-row text-raven mt-4  sm:px-4 gap-3 items-center'>
+             <h3 className='font-bold text-xl'>Filter</h3>
+                <select>
+                    <option>Sort: Recommended</option>
+                    <option>Higher Price</option>
+                    <option>Lower Price</option>
+                </select>
+        </div>
+        </div>
+    );
+};
+
+export default SortProducts;
