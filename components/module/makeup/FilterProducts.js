dd68148314c9005c3382eb876dceb72f2b8c8b08
Peggyta <peggyta9096@gmail.com>
2023-08-06 17:07:39 +0330

    arrows rotate onClick

--- components/module/makeup/FilterProducts.js
+++ components/module/makeup/FilterProducts.js
@@ -15,80 +15,80 @@ const FilterProducts = () => {
                 <p className='font-bold'>Out Of Stock Items</p>
                 <Toggle />
             </div>
-            <div className='mt-3 px-4 flex-col flex gap-5'>
+            <div className='mt-3 px-4 flex-col flex gap-6'>
                 <div>
                     <div className='flex justify-between cursor-pointer' onClick={() => setCategoryOpen(!categoryOpen)}>
                         <p className={categoryOpen ? 'text-lily font-bold': 'text-raven font-bold'}>Category</p>
-                        <ArrowDown />    
+                        <div className={categoryOpen ? 'stroke-lily rotate-180 transition' : 'stroke-raven'}><ArrowDown /></div>
                     </div>
                     <div className={categoryOpen ? 'my-3': 'hidden'}>
                         <div className='flex items-center gap-2'>
                             <input type="checkbox" className='accent-lily' />
-                            <p className='text-raven font-xl pt-0.5'>Cleansers</p>
+                            <p className='text-raven font-xl pt-0.5'>cleansers</p>
                         </div>
                         <div className='flex items-center pt-2 gap-2'>
                             <input type="checkbox" className='accent-lily' />
-                            <p className='text-raven font-xl pt-0.5'>Exfoliators</p>
+                            <p className='text-raven font-xl pt-0.5'>exfoliators</p>
                         </div>
                         <div className='flex items-center pt-2 gap-2'>
                             <input type="checkbox" className='accent-lily' />
-                            <p className='text-raven font-xl pt-0.5'>Toners</p>
+                            <p className='text-raven font-xl pt-0.5'>toners</p>
                         </div>
                         <div className='flex items-center pt-2 gap-2'>
                             <input type="checkbox" className='accent-lily' />
-                            <p className='text-raven font-xl pt-0.5'>Retinols</p>
+                            <p className='text-raven font-xl pt-0.5'>retinols</p>
                         </div>
                         <div className='flex items-center pt-2 gap-2'>
                             <input type="checkbox" className='accent-lily' />
-                            <p className='text-raven font-xl pt-0.5'>Night Cream</p>
+                            <p className='text-raven font-xl pt-0.5'>night Cream</p>
                         </div>
                     </div>
                 </div>
                 <div>
                 <div className='flex justify-between cursor-pointer' onClick={() => setSkinOpen(!skinOpen)}>
                         <p className={skinOpen ? 'text-lily font-bold': 'text-raven font-bold'}>Skin Condition</p>
-                        <ArrowDown />    
+                        <div className={skinOpen ? 'stroke-lily rotate-180 transition' : 'stroke-raven'}><ArrowDown /></div>   
                     </div>
                     <div className={skinOpen ? 'my-3': 'hidden'}>
                         <div className='flex items-center gap-2'>
                             <input type="checkbox" className='accent-lily' />
-                            <p className='text-raven font-xl pt-0.5'>Dry Skin</p>
+                            <p className='text-raven font-xl pt-0.5'>acne</p>
                         </div>
                         <div className='flex items-center pt-2 gap-2'>
                             <input type="checkbox" className='accent-lily' />
-                            <p className='text-raven font-xl pt-0.5'>Normal Skin</p>
+                            <p className='text-raven font-xl pt-0.5'>hydration</p>
                         </div>
                         <div className='flex items-center pt-2 gap-2'>
                             <input type="checkbox" className='accent-lily' />
-                            <p className='text-raven font-xl pt-0.5'>Greasy Skin</p>
+                            <p className='text-raven font-xl pt-0.5'>redness</p>
                         </div>
                     </div>
                 </div>
                 <div>
                 <div className='flex justify-between cursor-pointer' onClick={() => setFeaturedOpen(!featuredOpen)}>
                         <p className={featuredOpen ? 'text-lily font-bold': 'text-raven font-bold'}>Featured</p>
-                        <ArrowDown />    
+                        <div className={featuredOpen ? 'stroke-lily rotate-180 transition' : 'stroke-raven'}><ArrowDown /></div>   
                     </div>
                     <div className={featuredOpen ? 'my-3': 'hidden'}>
                         <div className='flex items-center gap-2'>
                             <input type="checkbox" className='accent-lily' />
-                            <p className='text-raven font-xl pt-0.5'>Anti-Pollution</p>
+                            <p className='text-raven font-xl pt-0.5'>new</p>
                         </div>
                         <div className='flex items-center pt-2 gap-2'>
                             <input type="checkbox" className='accent-lily' />
-                            <p className='text-raven font-xl pt-0.5'>Anti-Aging</p>
+                            <p className='text-raven font-xl pt-0.5'>best sellers</p>
                         </div>
                     </div>
                 </div>
                 <div>
                 <div className='flex justify-between cursor-pointer' onClick={() => setPriceOpen(!priceOpen)}>
                         <p className={priceOpen ? 'text-lily font-bold': 'text-raven font-bold'}>Price</p>
-                        <ArrowDown />    
+                        <div className={priceOpen ? 'stroke-lily rotate-180 transition' : 'stroke-raven'}><ArrowDown /></div>   
                     </div>
                     <div className={priceOpen ? 'my-3': 'hidden'}>
                         <div className='flex items-center gap-2'>
                             <input type="checkbox" className='accent-lily' />
-                            <p className='text-raven font-xl pt-0.5'>60 $ - 120 $</p>
+                            <p className='text-raven font-xl pt-0.5'>50 $ - 150 $</p>
                         </div>
                         <div className='flex items-center pt-2 gap-2'>
                             <input type="checkbox" className='accent-lily' />
@@ -96,11 +96,11 @@ const FilterProducts = () => {
                         </div>
                         <div className='flex items-center pt-2 gap-2'>
                             <input type="checkbox" className='accent-lily' />
-                            <p className='text-raven font-xl pt-0.5'>550 $ - 900 $</p>
+                            <p className='text-raven font-xl pt-0.5'>550 $ - 650 $</p>
                         </div>
                         <div className='flex items-center pt-2 gap-2'>
                             <input type="checkbox" className='accent-lily' />
-                            <p className='text-raven font-xl pt-0.5'>Up to 1000 $</p>
+                            <p className='text-raven font-xl pt-0.5'> 650 $ - 850 $</p>
                         </div>
                     </div>
                 </div>
