68072babddb95923fa613a09d3ad38ae752ba6d7
Peggyta <peggyta9096@gmail.com>
2023-10-19 20:33:26 +0330

    search styles edited

--- components/template/SearchBar.js
+++ components/template/SearchBar.js
@@ -3,7 +3,6 @@ import { useRouter } from 'next/router';
 import GreySearch from '@/components/icons/GreySearch';
 
 const SearchBar = () => {
-    const[isOpen, setIsOpen] = useState(false);
     const[products, setProducts] = useState('');
     const[type, setType] = useState('text');
 
@@ -15,6 +14,7 @@ const SearchBar = () => {
             alert('Enter name of a product!')
         }
     }
+    
     return (
         <>
         <div className='flex w-full px-10 items-center border-b border-ash'>
@@ -23,16 +23,13 @@ const SearchBar = () => {
             value={products}
             type={type} 
             onBlur={() => setType('text')}
-            onChange={(e) => setProducts(e.target.value)}
+            onChange={(e)=> setProducts(e.target.value)}
             className='w-full focus:outline-none py-4 bg-dust text-xl text-cement pl-3'  
             placeholder='What Are You Looking For?'
             />
             <button className='bg-jam transition px-5 rounded-lg hover:bg-cherry py-2 text-white' 
             onClick={searchHandler}>Search</button>
         </div>
-        <div onClick={()=> setIsOpen(!isOpen)} 
-          className={!products ? 'backdrop-blur-md brightness-50 min-h-screen absolute z-20 w-full left-0' : null}>
-        </div>   
         </>
     );
 };
